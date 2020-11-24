37d188a1909671856f29d464bed05036db7b702c
ShrutiRawal <shruti456rawal@gmail.com>
2020-11-24 16:17:10 +0530

    displays nearby medicine stores

--- app/src/main/java/com/example/pocketclinic/MainPageActivity.java
+++ app/src/main/java/com/example/pocketclinic/MainPageActivity.java
@@ -2,14 +2,30 @@ package com.example.pocketclinic;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainPageActivity extends AppCompatActivity {
+
+    private Button stores,doctors;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main_page);
         setContentView(R.layout.activity_main_page);
+        stores = (Button)findViewById(R.id.stores);
+        doctors = (Button)findViewById(R.id.doctors);
+        stores.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri gmmIntentUri = Uri.parse("geo:0,0?q=medicine stores");
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+                mapIntent.setPackage("com.google.android.apps.maps");
+                startActivity(mapIntent);
+            }
+        });
     }
 }
