6e83b9f89af8a22ccf3670a4e7768c6b2dc97579
ShrutiRawal <shruti456rawal@gmail.com>
2020-11-24 15:56:52 +0530

    Initial commit

---
+++ app/src/main/java/com/example/pocketclinic/MainPageActivity.java
@@ -0,0 +1,15 @@
+package com.example.pocketclinic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main_page);
+        setContentView(R.layout.activity_main_page);
+    }
+}
